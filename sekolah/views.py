add1203adaf2724acaa58d6cd0927fd2f432b522
rahadianhadi <rahadianhadi@gmail.com>
2023-01-25 11:52:21 +0700

    Tambah theme Majestic

--- sekolah/views.py
+++ sekolah/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index_response(request):
+    return HttpResponse("<h1>Hello World</h1>")
+
+def index(request):
+    var_nama = "Rahadian"
+    context = {
+        "nama": var_nama,
+    }
+
+    return render(request, "majesti/index.html", context)
